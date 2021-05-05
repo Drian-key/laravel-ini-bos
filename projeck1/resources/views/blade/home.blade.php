c55039feaab9d7da9c91e4674d7b912845036cdf
Drian-key <riskidrian1@gmail.com>
2021-05-05 13:51:19 +0700

    tambah asset dan bootsrap local

--- projeck1/resources/views/blade/home.blade.php
+++ projeck1/resources/views/blade/home.blade.php
@@ -5,4 +5,8 @@
 @section('content')
     <h1>ini halaman homes</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto tempora totam vel ea voluptate necessitatibus animi harum aperiam nobis nesciunt nulla officia eligendi, porro officiis eos consequuntur, saepe eaque vitae!</p>
+    <img src="{{asset('gambar/img1.png')}}" alt="" width="200px">
+    <img src="{{asset('gambar/img2.png')}}" alt="" width="200px">
+    <img src="{{asset('gambar/img3.png')}}" alt="" width="200px">
 @endsection
+ 
